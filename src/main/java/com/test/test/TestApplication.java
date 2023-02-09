ccd3b33d05f196a80f2544bd9463b509c490bb7f
hspark06 <hspark06@gmail.com>
2023-02-09 15:45:13 +0900

    testing branch second commit!

--- src/main/java/com/test/test/TestApplication.java
+++ src/main/java/com/test/test/TestApplication.java
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
-		System.out.println("111");
+		
 		SpringApplication.run(TestApplication.class, args);
+		System.out.println("111222");
 	}
 
 }
