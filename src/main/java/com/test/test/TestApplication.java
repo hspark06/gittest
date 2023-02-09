108007ef8ac620bba070d43da7feaa3247a8658b
hspark06 <hspark06@gmail.com>
2023-02-09 15:01:28 +0900

    made a change

--- src/main/java/com/test/test/TestApplication.java
+++ src/main/java/com/test/test/TestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("111");
 		SpringApplication.run(TestApplication.class, args);
 	}
 
